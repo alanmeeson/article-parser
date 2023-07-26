5f3741d1006b3fc5a4d548ab1abeb848a488278c
Alan Meeson <alan@carefullycalculated.co.uk>
2023-07-26 10:25:19 +0100

    Updated output_json to handle that figures or tables may not exist.
    Some articles will not contain tables or figures.  This adds a check to
    make sure that we don't try to write them when we don't have them.

--- article_parser/output_json.py
+++ article_parser/output_json.py
@@ -10,40 +10,42 @@ def output_json(document: str, out_path: str):
         os.makedirs(out_path)
 
     # save figures as png
-    num_figure_digits = int(np.ceil(np.log10(len(document['figures']))))
-    for figure_num, figure in enumerate(document['figures']):
-        figure_name = "figure%0*d.png" % (num_figure_digits, figure_num)
+    if document['figures']:
+        num_figure_digits = int(np.ceil(np.log10(len(document['figures']))))
+        for figure_num, figure in enumerate(document['figures']):
+            figure_name = "figure%0*d.png" % (num_figure_digits, figure_num)
 
-        if isinstance(figure['image'], np.ndarray):
-            figure_image = Image.fromarray(figure['image'])
-        elif isinstance(figure['image'], Image):
-            figure_image = figure['image']
-        else:
-            raise ValueError("Figure %0*d is neither a numpy array or a PIL Image." % (num_figure_digits, figure_num))
-        figure_image.save(os.path.join(out_path, figure_name), format='png')
+            if isinstance(figure['image'], np.ndarray):
+                figure_image = Image.fromarray(figure['image'])
+            elif isinstance(figure['image'], Image):
+                figure_image = figure['image']
+            else:
+                raise ValueError("Figure %0*d is neither a numpy array or a PIL Image." % (num_figure_digits, figure_num))
+            figure_image.save(os.path.join(out_path, figure_name), format='png')
 
-        figure['image'] = figure_name
+            figure['image'] = figure_name
 
     # save tables as png/csv
-    num_table_digits = int(np.ceil(np.log10(len(document['tables']))))
-    for table_num, table in enumerate(document['tables']):
-        table_name = "table%0*d" % (num_table_digits, table_num)
-        table_image_name = f"{table_name}.png"
-        table_csv_name = f"{table_name}.csv"
+    if document['tables']:
+        num_table_digits = int(np.ceil(np.log10(len(document['tables']))))
+        for table_num, table in enumerate(document['tables']):
+            table_name = "table%0*d" % (num_table_digits, table_num)
+            table_image_name = f"{table_name}.png"
+            table_csv_name = f"{table_name}.csv"
 
-        if isinstance(table['image'], np.ndarray):
-            table_image = Image.fromarray(table['image'])
-        elif isinstance(table['image'], Image):
-            table_image = table['image']
-        else:
-            raise ValueError("Image for Table %0*d is neither a numpy array or a PIL Image." % (num_table_digits, table_num))
+            if isinstance(table['image'], np.ndarray):
+                table_image = Image.fromarray(table['image'])
+            elif isinstance(table['image'], Image):
+                table_image = table['image']
+            else:
+                raise ValueError("Image for Table %0*d is neither a numpy array or a PIL Image." % (num_table_digits, table_num))
 
-        table_image.save(os.path.join(out_path, table_image_name), format='png')
-        table['image'] = table_image_name
+            table_image.save(os.path.join(out_path, table_image_name), format='png')
+            table['image'] = table_image_name
 
-        if table.get('table', None) is not None:
-            table['table'].to_csv(os.path.join(out_path, table_csv_name), index=False, header=False)
-            table['table'] = table_csv_name
+            if table.get('table', None) is not None:
+                table['table'].to_csv(os.path.join(out_path, table_csv_name), index=False, header=False)
+                table['table'] = table_csv_name
 
     # output the document as json
     document_json_file = os.path.join(out_path, 'document.json')

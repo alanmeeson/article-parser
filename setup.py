936a9e9f089cafac6a813647f76776675244fe80
Alan Meeson <alan@carefullycalculated.co.uk>
2023-07-30 11:57:00 +0100

    Introduce CHANGELOG.md & bump package version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ArticleParser',
-    version='0.1.0',
+    version='0.2.0',
     description='A tool for extracting text, figures and tables from academic papers.',
     author="Alan Meeson",
     author_email="alan@carefullycalculated.co.uk",
